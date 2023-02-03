547ee020d5a402539a0b14df3d9d5408d4bbc39b
gmarana <gmarana@umpisa.co>
2023-02-03 14:02:05 +0800

    first commit

---
+++ lib/core/view_model/auth_view_model.dart
@@ -0,0 +1,80 @@
+import 'package:ecommerce_app/core/service/firestore_user.dart';
+import 'package:ecommerce_app/model/user_model.dart';
+import 'package:ecommerce_app/view/home_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+class AuthViewModel extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String email, password, name;
+
+  final Rxn<User> _user = Rxn<User>();
+
+  String get user => _user.value?.email;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    _user.bindStream(_auth.authStateChanges());
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+  }
+
+  void signInWithEmailAndPassword() async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Get.offAll(HomeView());
+    } catch (e) {
+      print(e.message);
+      Get.snackbar(
+        'Error login account',
+        e.message,
+        colorText: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void createAccountWithEmailAndPassword() async {
+    try {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) async {
+        saveUser(user);
+      });
+
+      Get.offAll(HomeView());
+    } catch (e) {
+      print(e.message);
+      Get.snackbar(
+        'Error login account',
+        e.message,
+        colorText: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void saveUser(UserCredential user) async {
+    await FireStoreUser().addUserToFireStore(UserModel(
+      userId: user.user.uid,
+      email: user.user.email,
+      name: name ?? user.user.displayName,
+      pic: '',
+    ));
+  }
+}
